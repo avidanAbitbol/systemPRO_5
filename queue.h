ca3c543d173b5522999c997ba68c3528c233ee5e
avidan.abitbol <avidan88@walla.com>
2023-06-04 05:44:21 +0300

    Update queue.h

--- queue.h
+++ queue.h
@@ -1,24 +1,34 @@
-#ifndef QUEUE_H
-#define QUEUE_H
 
+#include <stdlib.h>
 #include <pthread.h>
+#define MUTEX_INIT(mutex) pthread_mutex_init(mutex, NULL)
+#define MUTEX_LOCK(mutex) pthread_mutex_lock(mutex)
+#define MUTEX_UNLOCK(mutex) pthread_mutex_unlock(mutex)
+#define MUTEX_DESTROY(mutex) pthread_mutex_destroy(mutex)
+#define COND_INIT(cond) pthread_cond_init(cond, NULL)
+#define COND_WAIT(cond, mutex) pthread_cond_wait(cond, mutex)
+#define COND_SIGNAL(cond) pthread_cond_signal(cond)
+#define COND_DESTROY(cond) pthread_cond_destroy(cond)
+#define ENQUEUE(queue, data) queueEnqueue(queue, (void *)data)
+#define DEQUEUE(queue, type) ((type)queueDequeue(queue))
 
-typedef struct QueueNode {
-    void* data;
-    struct QueueNode* next;
-} QueueNode;
+typedef struct _QueueNode {
+    void *data;
+    struct _QueueNode *next;
+} QueueNode, *PQueueNode;
 
-typedef struct Queue {
-    QueueNode* head;
-    QueueNode* tail;
-    pthread_mutex_t mutex;
+typedef struct _Queue {
+    PQueueNode head;
+    PQueueNode tail;
+    unsigned int size;
+    pthread_mutex_t lock;
     pthread_cond_t cond;
-} Queue;
+} Queue, *PQueue;
+PQueue queueCreate();
 
-Queue* createQueue();
-void enqueue(Queue* queue, void* item);
-void* dequeue(Queue* queue);
-void destroyQueue(Queue* queue);
-int isEmpty(Queue* q);
+void queueDestroy(PQueue queue);
+void queueEnqueue(PQueue queue, void *data);
+void *queueDequeue(PQueue queue);
+int queueIsEmpty(PQueue queue);
+int queueSize(PQueue queue);
 
-#endif /* QUEUE_H */
